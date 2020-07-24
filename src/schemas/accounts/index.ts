666ac4496b203fd744fd4f79d5f1d59f30c73df8
Vasily Novosad <delhsmail@gmail.com>
2020-07-24 16:20:29 +0500

    Build

--- src/schemas/accounts/index.ts
+++ src/schemas/accounts/index.ts
@@ -1,7 +1,7 @@
+import loaders from './loaders';
 import resolvers from './resolvers';
 import * as typeDefs from './schema.graphql';
 import Accounts from './service';
-import loaders from './loaders';
 
 export {
   typeDefs,
